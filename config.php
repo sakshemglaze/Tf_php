b5369e08322d074f3a06155e058794595f3a9dbe
sakshemglaze <sakshem@sakshemit.com>
2024-03-11 18:33:43 +0530

    more product by atul

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//define('BASE_URL', 'http://localhost/tf_result/');
-//define('BASE_URL', 'http://localhost/');
-//define('IMAGE_URL', 'https://doc.tradersfind.com/images/');
-//define('B_URL', '/tf_result');
-//define('BASE_URL', 'http://localhost/tf_result/');
-
-define('BASE_URL', 'http://localhost/');
+define('BASE_URL', 'http://localhost/tf_result/');
+ define('API_URL','https://api.tradersfind.com/');
 define('IMAGE_URL', 'https://doc.tradersfind.com/images/');
-define('API_URL','https://api.tradersfind.com/');
-define('B_URL', '');
+define('B_URL', '/tf_result');
+
+
+// define('BASE_URL', 'http://localhost/');
+// define('IMAGE_URL', 'https://doc.tradersfind.com/images/');
+// define('API_URL','https://api.tradersfind.com/');
+// define('B_URL', '');
 ?>
